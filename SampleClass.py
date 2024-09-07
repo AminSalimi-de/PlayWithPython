d3dc04cdf7f20ef9fa2652781fc3a74f6af40ec8
AminSalimi <amin.salimi.de@gmail.com>
2024-09-07 20:54:52 +0200

    Added Class Variables to the Employee

--- SampleClass.py
+++ SampleClass.py
@@ -1,14 +1,37 @@
 
 class Employee:
-    def __init__(self, first, last):
+    num_of_employees = 0
+    raise_coeff = 1.04
+    
+    def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.email = f"{first}.{last}@company.com"
-        
+        self.pay = pay
+        Employee.num_of_employees += 1
+           
     def fullName(self):
         return f"{self.first} {self.last}"
 
-emp1 = Employee("Amin", "Salimi")
+    def appply_raise(self):
+        self.pay *= self.raise_coeff
+        
+    def print(self):
+        print(f"Name={self.fullName()}")
+        print(f"email={self.email}")
+        print(f"raise_coeff={self.raise_coeff}")
+        print(f"pay={self.pay}")
+        print()
+        
+emp1 = Employee("Amin", "Salimi", 50000)
+emp2 = Employee("Taghi", "Tsghizadeh", 60000)
 
-print(emp1.fullName())
-print(emp1.email)
+emp1.raise_coeff = 1.05
+
+emp1.appply_raise()
+
+emp1.print()
+emp2.print()
+
+print(Employee.raise_coeff)
+print(Employee.num_of_employees)
